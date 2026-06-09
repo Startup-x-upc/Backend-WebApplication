21072f143293ad23f83ace5d4228327ea8edb7e8
sebastian <saiquipapoma@gmail.com>
2026-06-09 14:20:55 -0500

    feat: add ConfigureFarePolicyCommand

---
+++ src/main/java/org/example/backendwebapplication/monetization/domain/model/commands/ConfigureFarePolicyCommand.java
@@ -0,0 +1,9 @@
+package org.example.backendwebapplication.monetization.domain.model.commands;
+
+import java.math.BigDecimal;
+
+public record ConfigureFarePolicyCommand(
+        BigDecimal baseFare,
+        BigDecimal pricePerKm,
+        BigDecimal minimumFare
+) {}
