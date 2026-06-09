2d06301327f5e574883984b7067bba1d84893faf
sebastian <saiquipapoma@gmail.com>
2026-06-09 14:39:14 -0500

    feat: add WalletRepository interface

---
+++ src/main/java/org/example/backendwebapplication/monetization/domain/repositories/WalletRepository.java
@@ -0,0 +1,11 @@
+package org.example.backendwebapplication.monetization.domain.repositories;
+
+import org.example.backendwebapplication.monetization.domain.model.aggregates.Wallet;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface WalletRepository {
+    Wallet save(Wallet wallet);
+    Optional<Wallet> findByDriverId(UUID driverId);
+}
