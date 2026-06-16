25243dc5dd9f9e27e22c2d8019d64779f6a727e9
sebastian <saiquipapoma@gmail.com>
2026-06-16 10:37:49 -0500

    refactor(monetization): remove unused driverId field from TopUpWalletResource

--- src/main/java/org/example/backendwebapplication/monetization/interfaces/rest/resources/TopUpWalletResource.java
+++ src/main/java/org/example/backendwebapplication/monetization/interfaces/rest/resources/TopUpWalletResource.java
@@ -1,9 +1,7 @@
 package org.example.backendwebapplication.monetization.interfaces.rest.resources;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public record TopUpWalletResource(
-        UUID driverId,
         BigDecimal amount
 ) {}
