9f4458c122ef90a4d1c24a27f9f3131e0f2cc21e
apillacag <apillacag@gmail.com>
2026-06-18 23:55:48 -0500

    feat: add getfullnamebyuserid

--- src/main/java/org/example/backendwebapplication/iam/application/acl/IamContextFacadeImpl.java
+++ src/main/java/org/example/backendwebapplication/iam/application/acl/IamContextFacadeImpl.java
@@ -39,5 +39,10 @@ public class IamContextFacadeImpl implements IamContextFacade {
                 .map(user -> user.getRole());
     }
 
-
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<String> getFullNameByUserId(UUID userId) {
+        return profileRepository.findByUserId(userId)
+                .map(profile -> profile.getFullName());
+    }
 }
