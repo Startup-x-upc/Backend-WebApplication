b4adc0b451d30697bfdabe7bc0186d3366b77d01
TheEngineEdu <enginedujob@gmail.com>
2026-07-08 00:02:25 -0500

    fix:Solving problem with cancelled ride

--- src/main/java/org/example/backendwebapplication/ridedispatch/domain/model/events/RideCancelledEvent.java
+++ src/main/java/org/example/backendwebapplication/ridedispatch/domain/model/events/RideCancelledEvent.java
@@ -3,7 +3,7 @@ package org.example.backendwebapplication.ridedispatch.domain.model.events;
 import java.util.UUID;
 
 /**
- * Event published when a ride is cancelled before starting.
+ * Event published when a ride is canceled before starting.
  */
 public record RideCancelledEvent(
         UUID rideId,
